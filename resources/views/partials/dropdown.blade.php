6c219a55c6ee5ef4e0b5abdfd9c30f518eb9d630
Jim Hollington <jim@bozboz.co.uk>
2016-06-17 16:28:43 +0100

    Add 'please select' first option to  dropdown

--- resources/views/partials/dropdown.blade.php
+++ resources/views/partials/dropdown.blade.php
@@ -1,6 +1,11 @@
 {{ Form::label($field->name, $field->label) }}
-{!! Form::select($field->name, explode("\n", $field->options), null, array_filter([
-    'class' => 'form__input',
-    'required' => $field->required
-])) !!}
+{!! Form::select(
+    $field->name,
+    ['' => '- Please Select -'] + array_combine(explode("\n", $field->options), explode("\n", $field->options)),
+    null,
+    array_filter([
+        'class' => 'form__input',
+        'required' => $field->required
+    ])
+) !!}
 {!! $errors->first($field->name, '<div class="form__error">:message</div>') !!}
