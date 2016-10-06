a687a0240eee1af474369daa6af09b17d2873f24
Jim Hollington <jim@bozboz.co.uk>
2016-10-06 15:44:20 +0100

    Add partial for phone field

---
+++ resources/views/partials/phone.blade.php
@@ -0,0 +1,7 @@
+{{ Form::label($field->name, $field->label.($field->required ? ' *' : ''), ['class' => 'sr-only']) }}
+{{ Form::number($field->name, null, array_filter([
+    'class' => 'form__input',
+    'placeholder' => $field->placeholder,
+    'required' => $field->required
+])) }}
+{!! $errors->first($field->name, '<div class="form__error">:message</div>') !!}
