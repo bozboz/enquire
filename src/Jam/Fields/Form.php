659bab2b2e7bed15b03a6ede7420b85e72c2c565
Jim Hollington <jim@bozboz.co.uk>
2017-03-31 10:09:45 +0100

    Make jame form field extend belongsto

--- src/Jam/Fields/Form.php
+++ src/Jam/Fields/Form.php
@@ -12,37 +12,15 @@ use Bozboz\Jam\Fields\BelongsTo;
 
 class Form extends BelongsTo
 {
-    public function getAdminField(Entity $instance, EntityDecorator $decorator, Value $value)
+    protected function getRelationModel()
     {
-        if (property_exists($this->options_array, 'entity')) {
-
-            if (property_exists($this->options_array, 'make_parent')) {
-                if (!$instance->parent_id) {
-                    $instance->parent_id = $this->options_array->entity;
-                }
-                return new HiddenField($this->getInputName());
-            }
-
-            return new HiddenField($this->getInputName(), $this->options_array->entity);
-        }
-
-        return new BelongsToField($decorator, $this->relation($value), [
-            'name' => $this->getInputName(),
-            'label' => $this->getInputLabel(),
-            'help_text_title' => $this->help_text_title,
-            'help_text' => $this->help_text,
-        ]);
+        return Relation::class;
     }
 
     public function relation(Value $value)
     {
-        return $value->belongsTo(Relation::class, 'foreign_key')->with(['fields' => function($query) {
+        return parent::relation($value)->with(['fields' => function($query) {
             $query->orderBy('sorting');
         }]);
-    }
-
-    public function getOptionFields()
-    {
-        return [];
     }
 }
