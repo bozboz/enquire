1dc7b1f454f4e6741144fbc4b7f1417d0394cf6c
Jim Hollington <jim@bozboz.co.uk>
2017-11-20 17:24:21 +0000

    Remove regex from field label validation

--- src/Forms/Fields/FieldValidator.php
+++ src/Forms/Fields/FieldValidator.php
@@ -8,7 +8,7 @@ class FieldValidator extends Validator
 {
 	protected $rules = [
 		'form_id' => 'required|exists:enquiry_forms,id',
-        'label' => 'required|regex:/^[a-z0-9_]+$/|unique:enquiry_form_fields,label,{id},id,form_id,{form_id}',
+        'label' => 'required|unique:enquiry_form_fields,label,{id},id,form_id,{form_id}',
 		'input_type' => 'required',
 	];
 }
