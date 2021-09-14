7c579d5ab1210311c2fcfa0950583cb8aeec5091
Jim Hollington <jim@bozboz.co.uk>
2021-09-14 13:56:38 +0100

    Swap radio/label order

--- resources/views/partials/radios.blade.php
+++ resources/views/partials/radios.blade.php
@@ -4,8 +4,8 @@
         {{ Form::hidden($field->name, 'None chosen', ['id' => '']) }}
     @endif
     @foreach ($field->getOptions() as $option)
-        {{ Form::label(str_slug($field->name.$option), $option) }}
         {{ Form::radio($field->name, $option, null, ['id' => str_slug($field->name.$option)]) }}
+        {{ Form::label(str_slug($field->name.$option), $option) }}
     @endforeach
     {!! $errors->first($field->name, '<div class="form__error">:message</div>') !!}
 </fieldset>
